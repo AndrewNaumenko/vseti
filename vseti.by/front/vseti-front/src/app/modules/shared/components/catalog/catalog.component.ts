05aac8b0afc026552f6ea5c539f7ca968a6f1c36
danila16030 <danila.chechitov1603@gmail.com>
2020-04-27 19:45:06 +0300

    Add offer creation and reload

--- vseti.by/front/vseti-front/src/app/modules/shared/components/catalog/catalog.component.ts
+++ vseti.by/front/vseti-front/src/app/modules/shared/components/catalog/catalog.component.ts
@@ -27,9 +27,10 @@ export class CatalogComponent extends RxUnsubscribe implements OnInit {
 
   @Input()
   role: string;
+
   constructor(private http: HttpService, private formBuilder: FormBuilder, private filterService: FilteredOffersService,
               private loaderService: LoaderService, private router: Router, private dialog: MatDialog,
-              private updateService: UpdateService ) {
+              private updateService: UpdateService) {
     super();
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
     this.routerSubscription = this.router.events
@@ -42,5 +43,51 @@ export class CatalogComponent extends RxUnsubscribe implements OnInit {
           this.router.navigated = false;
         }
       });
+  }
+
+  ngOnInit(): void {
+    console.log(this.role);
+    this.isLoading.subscribe();
+    this.reloadOffers();
+    this.filterService.getFilteredOffers()
+      .pipe(
+        takeUntil((this.destroy$))
+      )
+      .subscribe(value => this.filteredOffersFromServer = value);
+    this.updateService.getMessageToUpdate()
+      .pipe(
+        takeUntil((this.destroy$))
+      )
+      .subscribe(value => {
+        if (value === true) {
+          this.reloadOffers();
+        }
+      });
+  }
+
+  reloadOffers(): void {
+    this.http.getOffers()
+      .pipe(
+        takeUntil((this.destroy$))
+      )
+      .subscribe(
+        (offersArray: OfferModel[]) => {
+          this.offersFromServer = offersArray;
+          this.filteredOffersFromServer = this.offersFromServer;
+        }
+      );
+  }
+
+  createOffer(): void {
+    console.log('create ');
+    this.dialog.open(OfferDialogComponent, {
+      data: {id: 0, title: '', description: '', photo: '', price: 0, category: {category: '', id: 0}}
+    }).afterClosed().subscribe(closeResponse => {
+      if (closeResponse === 'updated') {
+        console.log('call update');
+        // this.router.navigate(['admin']);
+        this.reloadOffers();
+      }
+    });
   }
 }
